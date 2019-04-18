f78a97455a082626da49c4d32c6b786facdd4306
catastrophe <sidthegame@gmail.com>
2019-04-18 11:33:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-gfq5ka
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-gfq5ka)
---
+++ app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  urls = new Array<string>();
+  detectFiles(event) {
+    this.urls = [];
+    let files = event.target.files;
+    if (files) {
+      for (let file of files) {
+        let reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.urls.push(e.target.result);
+        }
+        reader.readAsDataURL(file);
+      }
+    }
+  }
+}
